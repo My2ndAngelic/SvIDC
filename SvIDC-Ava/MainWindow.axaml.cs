19886632bff508a60a3311819bf060da789f1b23
My2ndAngelic <m2a@my2ndangelic.com>
2024-06-16 16:57:07 +0200

    Update

---
+++ SvIDC-Ava/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SvIDC_Ava;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SvIDC-Backend/IDCheck.cs
@@ -0,0 +1,36 @@
+﻿namespace SvIDC_Backend;
+
+public class IDCheck
+{
+    public static Boolean YearCheck(int year) => 1900 <= year && year <= 2100;
+
+    public static Boolean MonthCheck(int month) => 1 <= month && month <= 12;
+
+    public static Boolean DayCheck(int year, int month, int day)
+    {
+        // TODO: Check for leap year
+        
+        // TODO: Check for month
+        
+        // TODO: check for correct date
+        return true;
+    }
+
+    private static int ToYear4Digit(int year)
+    {
+        int currentYear = new DateTime().Year % 100;
+        switch (year)
+        {
+            case >= 1900 and <= 2100:
+                return year;
+            case >= 0 and <= 99:
+            {
+                return year < currentYear ? year + 1900 : year + 2000;
+            }
+            default:
+                throw new ArgumentException("Invalid year.");
+        }
+    }
+    
+    // TODO: Convert ID into array, then split: yy, mm, dd, xxx, c
+}
